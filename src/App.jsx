9a1d7eb16f2be59af7aa3e253cadf9fb468c83a8
Mohamed Ali <76428295+devbn3li@users.noreply.github.com>
2024-01-21 15:52:12 +0000

    update social media links in about page

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Mohamed Ali",
+  title: "Full Stack Developer",
+  email: "fuswhc@gmail.com",
+  gitHub: "devbn3li",
+  instagram: "devbn3li",
+  linkedIn: "devbn3li",
+  medium: "devbn3li",
+  twitter: "devbn3lii",
+  youTube: "devbn3li",
 };
 
 const primaryColor = "#4E567E";
