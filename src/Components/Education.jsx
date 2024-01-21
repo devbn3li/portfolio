69e0c888c52827a0533b113c8222b0c102fe7752
Mohamed Ali <76428295+devbn3li@users.noreply.github.com>
2024-01-21 16:43:23 +0000

    pushing to github

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -4,6 +4,23 @@ const Education = () => {
   return (
     <section className="light" id="education">
       <h2>Education</h2>
+      <div className="row">
+        <div className="col-4 col-6-medium col-12-small">
+          <h3>University of Toronto</h3>
+          <p>Full Stack Web Development</p>
+          <p>2021</p>
+        </div>
+        <div className="col-4 col-6-medium col-12-small">
+          <h3>University of Toronto</h3>
+          <p>Full Stack Web Development</p>
+          <p>2021</p>
+        </div>
+        <div className="col-4 col-6-medium col-12-small">
+          <h3>University of Toronto</h3>
+          <p>Full Stack Web Development</p>
+          <p>2024</p>
+        </div>
+      </div>
     </section>
   );
 };
