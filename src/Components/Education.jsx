b6d8716ecc1dc8fa5c439b28ced436e31d4cffc9
Mohamed Ali <76428295+devbn3li@users.noreply.github.com>
2024-02-12 21:50:03 +0000

    update the files

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -2,23 +2,18 @@ import React from "react";
 
 const Education = () => {
   return (
-    <section className="light" id="education">
+    <section className="flex center" id="education">
       <h2>Education</h2>
       <div className="row">
         <div className="col-4 col-6-medium col-12-small">
-          <h3>University of Toronto</h3>
-          <p>Full Stack Web Development</p>
+          <h3>Fayoum University</h3>
+          <p>English Languge</p>
           <p>2021</p>
         </div>
         <div className="col-4 col-6-medium col-12-small">
-          <h3>University of Toronto</h3>
-          <p>Full Stack Web Development</p>
-          <p>2021</p>
-        </div>
-        <div className="col-4 col-6-medium col-12-small">
-          <h3>University of Toronto</h3>
-          <p>Full Stack Web Development</p>
-          <p>2024</p>
+          <h3>ALX</h3>
+          <p>Full Stack Software Engineer</p>
+          <p>2023</p>
         </div>
       </div>
     </section>
